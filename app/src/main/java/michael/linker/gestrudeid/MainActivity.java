dda8b84ef6ca4f1e77829f27c813d6fe5d68c26e
Mikhail Linker <work.michaellinker@gmail.com>
2022-03-19 21:31:50 +0300

    Basic setup

---
+++ app/src/main/java/michael/linker/gestrudeid/MainActivity.java
@@ -0,0 +1,14 @@
+package michael.linker.gestrudeid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
