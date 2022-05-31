cd0d2b113b1328de544d66bfbefd4bd7b0672f4f
Mikhail Linker <work.michaellinker@gmail.com>
2022-06-01 00:57:08 +0300

    Typo

--- app/src/main/java/michael/linker/gestrudeid/stream/manager/StreamManagerNotFoundException.java
+++ app/src/main/java/michael/linker/gestrudeid/stream/manager/StreamManagerNotFoundException.java
@@ -4,10 +4,6 @@ package michael.linker.gestrudeid.stream.manager;
  * If required output/input stream was not found
  */
 public class StreamManagerNotFoundException extends RuntimeException {
-    public StreamManagerNotFoundException(String message) {
-        super(message);
-    }
-
     public StreamManagerNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
