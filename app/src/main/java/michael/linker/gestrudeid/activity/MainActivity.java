3e51708cc829d79c73c98ff811c618686c609837
Mikhail Linker <work.michaellinker@gmail.com>
2022-06-15 00:08:46 +0300

    Updated main activity layout

--- app/src/main/java/michael/linker/gestrudeid/activity/MainActivity.java
+++ app/src/main/java/michael/linker/gestrudeid/activity/MainActivity.java
@@ -1,10 +1,8 @@
-package michael.linker.gestrudeid;
+package michael.linker.gestrudeid.activity;
 
 import android.content.Context;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,7 +13,6 @@ import java.util.TimerTask;
 import michael.linker.gestrudeid.sensor.wrapper.manager.ASensorManager;
 import michael.linker.gestrudeid.sensor.wrapper.manager.SensorManagerWrapper;
 import michael.linker.gestrudeid.stream.output.model.FileOutputModel;
-import michael.linker.gestrudeid.stream.output.model.UiOutputModel;
 import michael.linker.gestrudeid.world.IWorld;
 import michael.linker.gestrudeid.world.exception.WorldFailedException;
 import michael.linker.gestrudeid.world.singleton.WorldSingleton;
@@ -25,26 +22,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         SensorManager hardwareSensorManager = (SensorManager) getSystemService(
                 Context.SENSOR_SERVICE);
         ASensorManager sensorManager = new SensorManagerWrapper(hardwareSensorManager);
 
-        TextView textView = this.findViewById(R.id.main_text_view);
-        textView.setMovementMethod(new ScrollingMovementMethod());
-
         File directory = this.getExternalFilesDir(null);
         String path = "";
 
-        UiOutputModel uiOutputModel = new UiOutputModel(textView);
-
         try {
             IWorld world = WorldSingleton.getInstance(sensorManager);
-            world.setNewOutputStream(uiOutputModel);
+
             new Timer().schedule(new TimerTask() {
                 @Override
                 public void run() {
+                    world.setNewOutputStream(
+                            new FileOutputModel(directory, path, "OutputFirst.txt"));
                     world.unsuppressRegistering();
                 }
             }, 5000);
@@ -54,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
                 public void run() {
                     world.suppressRegistering();
                     world.setNewOutputStream(
-                            new FileOutputModel(directory, path, "OutputFirst.txt"));
+                            new FileOutputModel(directory, path, "OutputSecond.txt"));
                     world.unsuppressRegistering();
                 }
             }, 10000);
@@ -63,20 +56,10 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     world.suppressRegistering();
-                    world.setNewOutputStream(
-                            new FileOutputModel(directory, path, "OutputSecond.txt"));
-                    world.unsuppressRegistering();
-                }
-            }, 15000);
-
-            new Timer().schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    world.suppressRegistering();
                     world.destroy();
                     exit();
                 }
-            }, 20000);
+            }, 15000);
         } catch (WorldFailedException e) {
             e.getStackTrace();
         }
