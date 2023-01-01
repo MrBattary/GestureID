8a277c291f73b2764f17d89f315c37c265023a6d
Mikhail Linker <work.michaellinker@gmail.com>
2023-01-01 15:57:42 +0300

    Removed useless activities

--- app/src/main/java/michael/linker/gestureid/elements/activity/MainActivity.java
+++ app/src/main/java/michael/linker/gestureid/elements/activity/MainActivity.java
@@ -1,17 +1,12 @@
 package michael.linker.gestureid.elements.activity;
 
 import android.content.Context;
-import android.content.Intent;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.File;
-
 import michael.linker.gestureid.R;
-import michael.linker.gestureid.elements.config.TestLoopConfiguration;
 import michael.linker.gestureid.sensor.wrapper.manager.ASensorManager;
 import michael.linker.gestureid.sensor.wrapper.manager.SensorManagerWrapper;
 import michael.linker.gestureid.world.IWorld;
@@ -26,13 +21,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        File directory = this.getExternalFilesDir(null);
-        TestLoopConfiguration.setDestination(directory);
-        TestLoopConfiguration.setTestLoopFolder(
-                "/Test".concat(String.valueOf(System.currentTimeMillis())));
-
-        buildButtons();
-
         SensorManager hardwareSensorManager = (SensorManager) getSystemService(
                 Context.SENSOR_SERVICE);
         ASensorManager sensorManager = new SensorManagerWrapper(hardwareSensorManager);
@@ -45,23 +33,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void buildButtons() {
-        Button start = findViewById(R.id.main__start);
-        start.setOnClickListener(view -> {
-            Intent activity = new Intent(MainActivity.this, ButtonActivity.class);
-            activity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            startActivity(activity);
-            finish();
-        });
-
-        Button exit = findViewById(R.id.main__exit);
-        exit.setOnClickListener(view -> closeApplication());
-    }
-
     private void closeApplication() {
-        world.suppressRegistering();
-        world.closeOutputStream();
         world.destroy();
-        this.finishAndRemoveTask();
+        ActivityGate.finishApplication(this);
     }
 }
