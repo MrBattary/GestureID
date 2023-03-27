f454532a96e602c40b2786e6d37913dc29c0d53c
Mikhail Linker <work.michaellinker@gmail.com>
2023-03-27 23:58:45 +0300

    Provided test navigation and finished test click layout

--- app/src/main/java/michael/linker/gestureid/ui/fragment/test/click/TestClickViewModel.java
+++
@@ -1,7 +0,0 @@
-package michael.linker.gestureid.ui.fragment.test.click;
-
-import androidx.lifecycle.ViewModel;
-
-public class TestClickViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
